988f758f4f4cc1c5ba516fad406ee58d449bd117
Caroline LS <carolinelds@gmail.com>
2022-04-25 12:30:31 -0300

    Implementa get para retornar os 10 últimos tweets publicados

--- src/app.js
+++ src/app.js
@@ -4,4 +4,23 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+let users = [];
+let tweets = [];
+
+app.post("/sign-up", (request,response) => {
+
+});
+
+app.post("/tweets", (request,response) => {
+
+});
+
+app.get("/tweets", (request,response) => {
+    let lastTen = [];
+    for (i = tweets.length - 1; i >= tweets.length - 10; i--){
+        lastTen.push(tweets[i]);
+    }
+    response.send(lastTen);
+});
+
 app.listen(5000, () => console.log("Server is running."));
