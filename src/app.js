0e7fd2bb432d14a0c4cb895a635380a6529354f1
Caroline LS <carolinelds@gmail.com>
2022-04-25 13:23:46 -0300

    Permite cadastro do usuáriona tela inicial

--- src/app.js
+++ src/app.js
@@ -1,24 +1,40 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(json());
 
 let users = [];
 let tweets = [];
 
-app.post("/sign-up", (request,response) => {
+app.post("/sign-up", (request, response) => {
+    const body = request.body;
 
+    const newUser = {
+        id: new Date().getTime(),
+        username: body.username,
+        avatar: body.avatar,
+    };
+
+    users.push(newUser);
+    response.send(users);
 });
 
-app.post("/tweets", (request,response) => {
-
+app.post("/tweets", (request, response) => {
+    const body = request.body;
 });
 
-app.get("/tweets", (request,response) => {
+app.get("/tweets", (request, response) => {
     let lastTen = [];
-    for (i = tweets.length - 1; i >= tweets.length - 10; i--){
-        lastTen.push(tweets[i]);
+    if (tweets.length < 10) {
+        for (let i = tweets.length - 1; i >= 0; i--) {
+            lastTen.push(tweets[i]);
+        }
+    } else {
+        for (let i = tweets.length - 1; i >= tweets.length - 10; i--) {
+            lastTen.push(tweets[i]);
+        }
     }
     response.send(lastTen);
 });
